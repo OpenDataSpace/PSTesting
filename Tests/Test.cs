d7764f560caaf8773eee9fa713948786a0cc371b
Stefan Burnicki <stefan.burnicki@burnicki.net>
2015-08-19 12:25:09 +0200

    Test project with first test of PSTesting

---
+++ Tests/Test.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using System;
+using PSTesting;
+
+namespace Tests
+{
+    [TestFixture]
+    public class Test : TestBase
+    {
+        [Test]
+        public void PostAndPreCommandsWork()
+        {
+            Shell.SetPostExecutionCommands("4", "5");
+            Shell.SetPreExecutionCommands("1", "2");
+            var res = Shell.Execute("3");
+            Assert.That(res, Is.EqualTo(new [] {1, 2, 3, 4, 5}));
+        }
+    }
+}
+
